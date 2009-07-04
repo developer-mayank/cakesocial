771a9119fca7d28ecba65a95074f15ad54bbf049
sardor90 <sardor90@c901e20a-b8d3-11dd-b612-3927438af0ba>
2009-07-04 17:53:02 +0000

    some changes ...

--- trunk/app/controllers/admin_controller.php
+++ trunk/app/controllers/admin_controller.php
@@ -1,6 +1,19 @@
 <?php
+/**
+ *
+ *
+ * @author pROCKrammer
+ * @email prockrammer@ya.ru
+ */
     class AdminController extends AppController
     {
         var $name = "Admin";
+        var $uses = array("Users");
+
+        function beforeFilter()
+        {
+            parent::beforeFilter();
+            $this -> Auth -> deny ();
+        }
     }
 ?>
