e4c3b4f9a8948bf3f19ca3084916b150757a44b9
sardor90 <sardor90@c901e20a-b8d3-11dd-b612-3927438af0ba>
2009-07-05 09:48:32 +0000

    Preparing for ACL ...

---
+++ trunk/app/models/usertype.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User Group (Admin, Modder or Just User)
+ *
+ * @author pROCKrammer (sardor90@gmail.com)
+ */
+class Usertype
+{
+    var $name = "Usertype";
+    
+    function parentNode()
+    {
+        return null;
+    }
+}
+?>
