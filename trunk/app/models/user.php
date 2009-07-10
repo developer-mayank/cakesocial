c098a2d7a78f56c5ab5bbf0d082f6c59e1fb2c5f
sardor90 <sardor90@c901e20a-b8d3-11dd-b612-3927438af0ba>
2009-07-10 07:41:40 +0000

    Preaparing to use Dark_Auth conmponent ... for ACL ...

--- trunk/app/models/user.php
+++ trunk/app/models/user.php
@@ -10,7 +10,7 @@
 class User extends AppModel {
 
     var $name = 'User';
-    var $actsAs = array('Acl' => array('requester'));
+
     
     var  $validate = array(
        'username' => array(
@@ -34,42 +34,10 @@ class User extends AppModel {
 	 );
 
      
-     var $hasAndBelongsToMany = array(  
-             'Group' => array('className' => 'Group',  
-                         'joinTable' => 'groups_users',  
-                         'foreignKey' => 'user_id',  
-                         'associationForeignKey' => 'group_id',  
-                        'unique' => true  
-            )  
-     );
-
-    var $belongsTo = array('Usertype');
+     var $hasAndBelongsToMany = array('Rank','Group');
 
 
-     function parentNode()
-     {
-         if (!$this->id && empty($this->data))
-         {
-            return null;
-         }
 
-         $data = $this->data;
-
-         if (empty($this->data))
-         {
-             $data = $this->read();
-         }
-
-
-         if (!$data['User']['usertype_id'])
-         {
-            return null;
-         }
-         else
-         {
-            return array('Group' => array('id' => $data['User']['usertype_id']));
-         }
-     }
 		/*
 		var $hasAndBelongsToMany = array(
         'ForwardRelation' => array(
